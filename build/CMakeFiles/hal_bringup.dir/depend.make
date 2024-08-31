50fc3e370709634e4073326fae8ff3b9140374e2
Justin Valentine <JustinValentineCS@gmail.com>
2024-08-30 23:32:11 -0600

    Remove build directory and other ignored files from tracking

--- build/CMakeFiles/hal_bringup.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for hal_bringup.
-# This may be replaced when dependencies are built.
--- build/CMakeFiles/hal_bringup.dir/src/imu.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/hal_bringup.dir/src/imu.o: /home/thor/Desktop/HAL/src/imu.cc \
- /usr/include/stdc-predef.h
